214b3c462cb3745c29329c0e8f1043bb71d0b8a7
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-24 13:30:35 -0300

    refactor listBeers feature

--- src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
@@ -6,6 +6,7 @@ import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
@@ -17,6 +18,15 @@ public interface BeerRepository extends ReactiveCrudRepository<Beer, Integer> {
 //    Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
 //
 //    Page<Beer> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+
+    //These do not work, but may in future
+//    Flux<Page<Beer>> findAllByBeerName(String beerName, Pageable pageable);
+//
+//    Flux<Page<Beer>> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
+//
+//    Flux<Page<Beer>> findAllByBeerNameAndBeerStyle(String beerName, BeerStyleEnum beerStyle, Pageable pageable);
+//
+//    Flux<Page<Beer>> findBeerBy(Pageable pageable);
 
     Mono<Beer> findByUpc(String upc);
 }
