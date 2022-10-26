62b9d68a213cc17f9a8dab3e959696a011f076a8
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2022-10-26 11:55:59 -0300

    add v2 getBeerById

---
+++ src/main/java/guru/springframework/sfgrestbrewery/web/functional/BeerRouterConfig.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgrestbrewery.web.functional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+
+@Configuration
+public class BeerRouterConfig {
+    public static final String BEER_V2_PATH = "/api/v2/beer/";
+
+    @Bean
+    public RouterFunction<ServerResponse> beerRoutesV2(BeerHandlerV2 handler) {
+        return route().GET(BEER_V2_PATH + "{beerId}", accept(APPLICATION_JSON),
+                handler::getBeerById).build();
+    }
+}
